36feef73d79213eb90a933ccd535b2f7d430d05c
Artem Vanakov <artem.vanakov@i3design.com>
2018-01-04 15:56:52 +0200

    refac

--- src/components/calendar/day.js
+++ src/components/calendar/day.js
@@ -9,6 +9,15 @@ export class Day extends React.Component {
 		return (nextProps.events !== this.props.events)
 	}
 	render() {
+		let i = 6000;
+		console.time('fun')
+		function recur() {
+			if(i==0) {return i}
+			i--;
+			recur(i/0.00000234*1000000/2300000000000)
+		}
+		recur();
+		console.timeEnd('fun')
 		console.log(`render ${this.props.id}`);
 		return (
 			<div className="day">
